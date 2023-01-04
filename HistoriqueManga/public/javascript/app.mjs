906896948f3ea467a9303ccebfa0885733bd509e
LePangolin <91209500+LePangolin@users.noreply.github.com>
2023-01-04 18:49:38 +0100

    Initial Commit

---
+++ HistoriqueManga/public/javascript/app.mjs
@@ -0,0 +1,17 @@
+import navbaritem from "./components/navBar.mjs";
+
+const app = Vue.createApp({
+    data() {
+        return {
+            // ...
+        }
+    },
+    methods: {
+        // ...
+    },
+    components: {
+        navbaritem
+    }
+});
+
+app.mount('#container');
